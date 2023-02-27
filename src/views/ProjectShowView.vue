05e0388d483affe2d6679b00284d1ae98eb546f4
RubenVG <rmvang01@gmail.com>
2023-02-27 22:57:44 +0100

    #2 add project show view vue

---
+++ src/views/ProjectShowView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="projectsShow">
+      <h1>This is the projects page for project: {{ $route.params.id }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
