c348210f80f1e90dc5dbecc129b46ff433d23616
RubenVG <rmvang01@gmail.com>
2023-03-06 23:10:40 +0100

    #3 added project show view using projects.json

--- src/views/ProjectShowView.vue
+++ src/views/ProjectShowView.vue
@@ -5,6 +5,44 @@
 </template>
 
 <script lang="ts">
-export default {}
+
+import { defineComponent } from "vue";
+import Project from "@/types/Project";
+import ProjectTag from "@/types/ProjectTag";
+import ProjectLanguage from "@/types/ProjectLanguage";
+import ProjectData from '@/data/projects.json';
+import { useRoute } from "vue-router";
+
+export default defineComponent ({
+	name: 'ProjectShowView',
+	setup() {
+    
+  const route = useRoute()
+  const projectId = route.params.id;
+
+  let projectItem;
+
+		try {
+				const project = ProjectData["Project " + projectId as keyof typeof ProjectData];
+				const projectObject: Project = {
+					id: project.id,
+					name: project.name,
+					description: project.description,
+					start: new Date(project.start),
+					end: new Date(project.end),
+					tags: project.tags.map((tag: string) => tag as ProjectTag),
+					languages: project.languages.map((language: string) => language as ProjectLanguage),
+					location: project.location,
+					grade: project.grade,
+				};
+        projectItem = projectObject;
+		} catch (error) {
+			console.error(error);
+		}	
+  return { projectItem }
+		
+	}
+})
+
 </script>
 
